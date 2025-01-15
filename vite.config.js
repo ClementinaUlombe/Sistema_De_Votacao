bd60cd1e2a449c770da6bd510a153bc228d1cfd8
Clementina Ulombe <clementina.ulombe@sintaxy.com>
2025-01-15 15:06:44 +0100

    dep

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,8 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/Sistema_De_Votacao/', 
+
   resolve: {
     extensions: ['.js', '.jsx', '.json']
   }
